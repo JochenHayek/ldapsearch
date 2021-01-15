5653e9dd6257cb60500a5c4b677dbccc907af1d7
Jochen Hayek <jochen@hayek.name>
2021-01-16 00:13:44 +0100

    *** empty log message ***

---
+++ ldapsearch.wrapper-___.sh
@@ -0,0 +1,12 @@
+:
+
+# $ env ldap_password='...' ~/bin/ldapsearch.wrapper-___.sh > ~/transfer/ldap.$( date '+%Y%m%d%H%M%S' ).txt
+
+env \
+  \
+  ldapuri='ldap://ldaps.___' \
+  ldap_binddn='CN=___,OU=___,DC=___,DC=local' \
+  ldap_searchbase='ou=___,ou=___,dc=___,dc=local' \
+  \
+  ~/git-servers/github.com/JochenHayek/ldapsearch/ldapsearch.sh \
+  ;
